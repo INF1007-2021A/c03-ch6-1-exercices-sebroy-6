6a979b434a5586f593b6c3e53d40eba9dc89326a
sebroy-6 <sebastien-6.roy@polymtl.ca>
2021-09-27 19:25:23 -0400

    Completed Version

--- exercice.py
+++ exercice.py
@@ -5,58 +5,132 @@
 def order(values: list = None) -> list:
     if values is None:
         # TODO: demander les valeurs ici
-        pass
 
-    return []
+        values = list()
+
+        while len(values) != 10:
+            print("Veuiller s'il vous plait insérer 10 valeurs avec des espaces entre elles: \n")
+            values = input().split(" ")
+
+            if len(values) != 10:
+                print("Vous n'avez pas inscrit les variables correctement, veuillez réessayer")
+                values = list()
+
+    return sorted(values)
 
 
 def anagrams(words: list = None) -> bool:
     if words is None:
         # TODO: demander les mots ici
-        pass
 
-    return False
+        words = []
+        print("Veuillez inscrire le premier mot : \n")
+        words.append(input())
 
+        print("Veuillez inscrire le deuxième mot : \n")
+        words.append(input())
+
+        for letter in words[0]:
+            if letter not in words[1]:
+                return False
+            else:
+                return True
+
+    else:
+        for letter in words[0]:
+            if letter not in words[1]:
+                return False
+            else:
+                return True
 
 def contains_doubles(items: list) -> bool:
-    return False
+
+    return False if list(set(items)) == items else True
 
 
 def best_grades(student_grades: dict) -> dict:
     # TODO: Retourner un dictionnaire contenant le nom de l'étudiant ayant la meilleure moyenne ainsi que sa moyenne
-    return {}
+
+    averages = 0
+    best_average = 0
+    best_student = ""
+
+    for name, grades in student_grades.items():
+        for note in grades:
+            averages += note
+        averages /= (len(grades))
+        student_grades[name] = averages
+        averages = 0
+
+    for student in student_grades:
+        if student_grades[student] > best_average:
+            best_average = student_grades[student]
+            best_student = student
+
+    return {best_student : best_average}
 
 
 def frequence(sentence: str) -> dict:
     # TODO: Afficher les lettres les plus fréquentes
     #       Retourner le tableau de lettres
 
-    return {}
+    letter_dictionnary = dict()
+    for letter in sentence:
+        if letter in letter_dictionnary:
+            letter_dictionnary[letter] += 1
+        else:
+            letter_dictionnary[letter] = 1
+    return letter_dictionnary
 
 
 def get_recipes():
     # TODO: Demander le nom d'une recette, puis ses ingredients et enregistrer dans une structure de données
-    pass
+
+    livre_recette = dict()
+
+    print("combien de recettes voulez vous ajouter?")
+    nb_entries = int(input())
+
+    for i in range(nb_entries):
+        print(f"Quel est le nom de votre recette numéro {i}: ")
+        recette = input()
+
+        print("Quels sont les ingrédients de cette recette "
+              "(laissez un espace entre chaque ingrédient): ")
+        ingredients = input().split(" ")
+
+        livre_recette[recette] = ingredients
+
+    return livre_recette
 
 
 def print_recipe(ingredients) -> None:
     # TODO: Demander le nom d'une recette, puis l'afficher si elle existe
-    pass
+    recette = ""
+
+    while recette not in ingredients:
+
+        print("Quel est le nom de la recette que vous cherchez: ")
+        recette = input()
+        if recette in ingredients:
+            print(f"Voici les ingrédients de la recette {recette}:", ingredients[recette])
+        else:
+            print("Désolé, mais cette recette n'est pas dans le livre de recette")
 
 
 def main() -> None:
     print(f"On essaie d'ordonner les valeurs...")
     order()
 
-    print(f"On vérifie les anagrammes...")
-    anagrams()
+    #print(f"On vérifie les anagrammes...")
+    #anagrams()
 
     my_list = [3, 3, 5, 6, 1, 1]
     print(f"Ma liste contient-elle des doublons? {contains_doubles(my_list)}")
 
     grades = {"Bob": [90, 65, 20], "Alice": [85, 75, 83]}
     best_student = best_grades(grades)
-    print(f"{list(best_student.keys())[0]} a la meilleure moyenne: {list(best_student.values())[0]}")
+#    print(f"{list(best_student.keys())[0]} a la meilleure moyenne: {list(best_student.values())[0]}")
 
     sentence = "bonjour, je suis une phrase. je suis compose de beaucoup de lettre. oui oui"
     frequence(sentence)
